934a608b0d62b9030e63985fb4cfce62a177a1f3
meleneym44-lab <meleneym44@gmail.com>
2026-01-29 19:12:56 +0100

    Update and rename page.js.old to page.js

---
+++ app/page.js
@@ -0,0 +1,17 @@
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Home() {
+  const router = useRouter();
+  
+  useEffect(() => {
+    router.push('/customer');
+  }, [router]);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p>Redirecting...</p>
+    </div>
+  );
+}
